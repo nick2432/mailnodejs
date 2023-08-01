60accd1589013d1d09780ab2ff1e132e905175a7
nick2432 <77477551+nick2432@users.noreply.github.com>
2023-08-01 12:00:54 +0530

    Update101 router.js

--- routes/router.js
+++ routes/router.js
@@ -11,12 +11,21 @@ router.post("/register",(req,res)=>{
                 pass:"heppviwbgxxuuqae"
             }
         });
-        const mailOptions = {
-            from: "nikhilanand2432@gmail.com",
-            to : email,
-            subject:"lsdfkjdlsfj",
-            html:`<h1>${name},${no},${email1},${message}<h1>`
-        }
+          const htmlBody = `
+      <h1>Hello ${name},</h1>
+      <p>Thank you for your message!</p>
+      <p>Your mobile number: ${no}</p>
+      <p>Your email address: ${email1}</p>
+      <p>Message: ${message}</p>
+    `;
+
+    const mailOptions = {
+      from: "nikhilanand2432@gmail.com",
+      to: email,
+      subject: "Thank you for your message",
+      html: htmlBody,
+    };
+
         transporter.sendMail(mailOptions,(error,info)=>{
             if(error){
                 console.log("Error",error);
